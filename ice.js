8e89d4f8e61991baebe404e31e7b76dff7aa04a3
Dominic Vonk <info@dovocode.com>
2016-07-25 17:03:30 +0200

    Update ice.js

--- ice.js
+++ ice.js
@@ -1,73 +1,42 @@
-(function(w) {
-    'use strict';
-    var ice = {
-        cone: 'ice',
-        structure: '[%cone%-%taste%]',
-        machineRunning: false,
-        coneWithTaste: function(taste, consumer) {
-            if (typeof consumer !== "undefined") {
-                return this.structure.replace('%cone%', this.cone).replace('%taste%', [taste, '="', consumer, '"'].join(''));
-            } else {
-                return this.structure.replace('%cone%', this.cone).replace('%taste%', taste);
-            }
-        },
-        machine: function() {
-            this.machineRunning = true;
-            $.each(this.tastes, function(taste, events) {
-             $.each(events, function(event, func) {
-                    $(w.ice.coneWithTaste(taste)).off(event, func).on(event, func);
-                });
-            });
-        },
-        stopMachine: function() {
-            $.each(this.tastes, function(taste, events) {
-                $.each(events, function(event, func) {
-                    $(ice.coneWithTaste(taste)).off(event, func);
-                });
-            });
-            this.machineRunning = false;
-        },
-        tastes: {},
-        refreshMachine: function() {
-            this.machine();
-        },
-        addTaste: function(taste, on, functionality) {
-            if (typeof this.tastes === "undefined") {
-                this.tastes = {};
-            }
-            if (typeof this.tastes[taste] === "undefined") {
-                this.tastes[taste] = {};
-            }
-            if (typeof on === "string") {
-                this.tastes[taste][on] = functionality;
-            } else {
-                $.each(on, function(key, val) {
-                    ice.tastes[taste][key] = val;
-                });
-            }
-            if (this.machineRunning) {
-                this.machine();
-            }
-        },
-        deleteTaste: function(taste, on) {
-            if (this.machineRunning) {
-                if (typeof on !== "undefined") {
-                    $(ice.coneWithTaste(taste)).off(on, this.tastes[taste][on]);
-                } else {
-                    $.each(this.tastes[taste], function(event, func) {
-                        $(ice.coneWithTaste(taste)).off(event, func);
-                    });
-                }
-            }
-            if (typeof on !== "undefined") {
-                delete this.tastes[taste][on];
-            } else {
-                delete this.tastes[taste];
-            }
-            if (this.machineRunning) {
-                this.machine();
-            }
-        }
-    };
-    w.ice = ice;
-}(window));
+var ice = {
+  prefix: 'ice',
+  running: false,
+  tastes: {},
+  addTaste: function(suffix, events) {
+    this.tastes[suffix] = events;
+    if (ice.running) {
+      ice.restartMachine();
+    }
+  },
+  removeTaste: function(suffix, event) {
+    if (typeof event === "undefined") {
+      delete ice.tastes[suffix];
+    } else {
+      delete ice.tastes[suffix][event];
+    }
+    if (ice.running) {
+      ice.restartMachine();
+    }
+  },
+  startMachine: function() {
+    $.each(ice.tastes, function(suffix, events) {
+        $.each(events, function(name, event) {
+            $('['+ice.prefix+'-'+suffix+']').on(name, event);
+        });
+    });
+    ice.running = true;
+  },
+  stopMachine: function() {
+    $.each(ice.tastes, function(suffix, events) {
+        $.each(events, function(name, event) {
+            $('['+ice.prefix+'-'+suffix+']').off(name);
+        });
+    });
+    ice.running = false;
+  },
+  restartMachine: function() {
+    ice.stopMachine();
+    ice.startMachine();
+  }
+  
+};
