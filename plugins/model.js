a349b268b940d696fa2b7ab89f4aa90924c3cfa7
Dominic Vonk <info@dovocode.com>
2016-07-25 17:10:13 +0200

    Update model.js

--- plugins/model.js
+++ plugins/model.js
@@ -1,6 +1,6 @@
 ice.addTaste("model", {
 	"input" : function() {
-		var model = '[' + ice.prefix + '-model=' + $(this).attr('[' + ice.prefix + '-model]') + '"]';
+		var model = '[' + ice.prefix + '-model="' + $(this).attr(ice.prefix + '-model') + '"]';
 		$(model).html($(this).val());
 		$(model).val($(this).val());
 	}
